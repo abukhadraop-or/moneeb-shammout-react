794bdfc9a6f3476f11cf2e52e7ad6754bd6d286c
moneebshammout <moneeb41@gmail.com>
2022-08-16 18:59:56 +0300

    removed proxy added a fixed endpoint

--- src/service/movies.service.js
+++ src/service/movies.service.js
@@ -8,10 +8,13 @@
  */
 const fetchMovies = async (pageNumber, sortType) => {
   try {
-    const response = await fetch(`/movies/${pageNumber}&${sortType}`, {
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      method: 'GET',
-    });
+    const response = await fetch(
+      `http://localhost:3000/movies/${pageNumber}&${sortType}`,
+      {
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        method: 'GET',
+      }
+    );
     return response;
   } catch (error) {
     return error;
